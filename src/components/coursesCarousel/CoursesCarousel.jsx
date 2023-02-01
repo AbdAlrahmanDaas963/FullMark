db37aea769b31c7f2a941fbdcb9c7939b90ccccd
Abdalrahman <abdalrahmandaas963@gmail.com>
2023-02-01 18:15:49 +0300

    drag added

--- src/components/coursesCarousel/CoursesCarousel.jsx
+++ src/components/coursesCarousel/CoursesCarousel.jsx
@@ -6,6 +6,7 @@ import "./coursesCarousel.css";
 import CourseCard from "./CourseCard";
 
 function CoursesCarousel() {
+  const [carouselWidth, setCarouselWidth] = useState();
   const [state, setState] = useState([
     {
       id: 1,
@@ -32,7 +33,27 @@ function CoursesCarousel() {
       <div className="corses-carousel-container">
         <div className="big-text">Your Courses</div>
         <div className="carousel-scroll">
-          <motion.div className="carousel-in" drag="x" dragDirectionLock>
+          <motion.div
+            ref={(el) => {
+              if (!el) return;
+
+              console.log(el.getBoundingClientRect().width); // prints 200px
+              setCarouselWidth(el.getBoundingClientRect().width);
+            }}
+            className="carousel-in"
+            drag="x"
+            whileTap={{ cursor: "grabbing" }}
+            whileHover={{ cursor: "grab" }}
+            dragDirectionLock
+            dragConstraints={{
+              top: 0,
+              right: 0,
+              bottom: 0,
+              left: -carouselWidth + 200,
+            }}
+            dragTransition={{ bounceStiffness: 600, bounceDamping: 20 }}
+            dragElastic={0.5}
+          >
             {state.map((item) => (
               <CourseCard
                 key={item.id}
