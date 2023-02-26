ae5192b391e936c5e3a2839d687f44a7d9058c30
Abdalrahman <abdalrahmandaas963@gmail.com>
2023-02-27 00:34:08 +0300

    up

--- src/screens/login/Login.jsx
+++ src/screens/login/Login.jsx
@@ -10,6 +10,7 @@ import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
 import Divider from "@mui/material/Divider";
+import Alert from "@mui/material/Alert";
 
 import GoogleIcon from "@mui/icons-material/Google";
 import AppleIcon from "@mui/icons-material/Apple";
@@ -49,56 +50,73 @@ function Login() {
   );
 
   return (
-    <Container maxWidth="sm" className="login">
-      <form onSubmit={formik.handleSubmit} className="login-form">
-        <Stack spacing={2} justifyContent="center" alignItems="center">
-          <h1>Welcome Back</h1>
-          <TextField
-            id="outlined-basic-email"
-            label="E-mail"
-            variant="outlined"
-            type="email"
-            autoComplete="true"
-            value={formik.values.email}
-            onChange={(e) => setInputValue("email", e.target.value)}
-          />
-          <small>{formik.errors.email}</small>
-
-          <TextField
-            id="outlined-basic-pass"
-            label="Password"
-            variant="outlined"
-            type="password"
-            value={formik.values.password}
-            onChange={(e) => setInputValue("password", e.target.value)}
-          />
-          <small>{formik.errors.password}</small>
-          {!!formik.errors.password && <br />}
-          <Button variant="contained" type="submit" disabled={!formik.isValid}>
-            Login
-          </Button>
-          <div>
-            don’t have account?{" "}
-            <Link to={"/signup"}>
-              <Button>sign up</Button>
-            </Link>
-          </div>
-          <Divider style={{ width: "100%" }} color="#363759">
-            OR
-          </Divider>
+    <div className="login-back">
+      <Container className="login">
+        <form onSubmit={formik.handleSubmit} className="login-form">
           <Stack
-            width={"100%"}
-            direction={"row"}
-            alignItems={"center"}
-            justifyContent={"space-evenly"}
+            spacing={2}
+            flex={"1"}
+            justifyContent="center"
+            alignItems="center"
           >
-            <GoogleIcon className="logins-icons" htmlColor="#fff" />
-            <AppleIcon className="logins-icons" htmlColor="#fff" />
-            <FacebookIcon className="logins-icons" htmlColor="#fff" />
+            <h1>Welcome Back</h1>
+            <TextField
+              error={formik.errors.email}
+              id="standard-error-helper-text"
+              label="E-mail"
+              defaultValue="Hello World"
+              helperText={formik.errors.email ? formik.errors.email : ""}
+              variant="standard"
+              type="email"
+              autoComplete="true"
+              value={formik.values.email}
+              onChange={(e) => setInputValue("email", e.target.value)}
+            />
+
+            <TextField
+              error={formik.errors.password}
+              id="standard-error-helper-text"
+              label="Password"
+              defaultValue="Hello World"
+              helperText={formik.errors.password ? formik.errors.password : ""}
+              variant="standard"
+              type="password"
+              autoComplete="true"
+              value={formik.values.password}
+              onChange={(e) => setInputValue("password", e.target.value)}
+            />
+
+            {!!formik.errors.password && <br />}
+            <Button
+              variant="contained"
+              type="submit"
+              disabled={!formik.isValid}
+            >
+              Login
+            </Button>
+            <div>
+              don’t have account?{" "}
+              <Link to={"/signup"}>
+                <Button>sign up</Button>
+              </Link>
+            </div>
+            <Divider style={{ width: "100%" }} color="#363759">
+              OR
+            </Divider>
+            <Stack
+              width={"100%"}
+              direction={"row"}
+              alignItems={"center"}
+              justifyContent={"space-evenly"}
+            >
+              <GoogleIcon className="logins-icons" htmlColor="#fff" />
+              <AppleIcon className="logins-icons" htmlColor="#fff" />
+              <FacebookIcon className="logins-icons" htmlColor="#fff" />
+            </Stack>
           </Stack>
-        </Stack>
-      </form>
-    </Container>
+        </form>
+      </Container>
+    </div>
   );
 }
 
